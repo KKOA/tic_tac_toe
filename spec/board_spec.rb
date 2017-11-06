4813e1b26050ce5f95a27099483a282541cee8aa
keith(mac) <ka33145@googlemail.com>
2017-11-06 23:18:19 +0000

    Add test for on_grid on board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,4 +11,20 @@ describe Board do
       expect(board.grid).to eq expect_val
     end
   end
+  describe '#on_grid' do
+    context 'coordinates on the grid' do
+      row = 0
+      col = 0
+      it 'reurn true' do
+        expect(board.on_grid?(row, col)).to eq true
+      end
+    end
+    context 'coordinates off the grid' do
+      row = 3
+      col = 3
+      it 'return false' do
+        expect(board.on_grid?(row, col)).to eq false
+      end
+    end
+  end
 end
