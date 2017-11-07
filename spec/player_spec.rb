ffa79fcda39adfdd63af7f6f79130c1c92166614
keith(mac) <ka33145@googlemail.com>
2017-11-07 21:36:16 +0000

    Remove uncessary test from player_spec

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,15 +2,12 @@ require 'player'
 describe Player do
   subject(:player1) { described_class.new('Tom') }
   describe '#name' do
-    it 'has method' do
-      expect(player1).to respond_to(:name)
-    end
     it 'return player name' do
       expect(player1.name).to eq 'Tom'
     end
   end
   describe '#flag' do
-    it 'return player name' do
+    it 'return player flag' do
       player1.flag = 'x'
       expect(player1.flag).to eq 'x'
     end
