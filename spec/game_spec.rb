27ce5f6932594f98436a1fa3a648b19e8f9e8e7f
keith(mac) <ka33145@googlemail.com>
2017-11-07 16:15:12 +0000

    Added test for cell already taken

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,10 @@
 require 'game'
 require 'board'
 describe Game do
-  let(:player1) { double(Player, name: 'tom') }
-  let(:player2) { double(Player, name: 'mary') }
+  let(:player1) { double(Player, name: 'tom', flag: 'x') }
+  let(:player2) { double(Player, name: 'mary', flag: 'o') }
+  # allow​(player1).to receive(​:flag) {​ 'x' ​}
+
   let(:board) { Board.new }
   subject(:game) { described_class.new(board, player1, player2) }
 
@@ -13,13 +15,13 @@ describe Game do
     end
   end
 
-  describe '#place_piece' do
+  describe '#claim' do
     context 'on board empty' do
       it 'with valid coordinates' do
         row = 0
         col = 0
-        expect_val = [['x','-','-'],['-','-','-'],['-','-','-']]
-        expect(game.place_piece(row, col)).to eq expect_val
+        expect_val = [['x', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+        expect(game.claim(row, col)).to eq expect_val
         expect(game.current_player.name).to eq player2.name
       end
 
@@ -27,27 +29,27 @@ describe Game do
         row = 5
         col = 3
         expect_val = "#{row} , #{col} is an invalid cell. Please try again."
-        expect(game.place_piece(row, col)).to eq expect_val
+        expect(game.claim(row, col)).to eq expect_val
       end
     end
     context 'Other player turn' do
       it 'with valid coordinates' do
         row = 0
         col = 0
-        game.place_piece(row, col)
+        game.claim(row, col)
         row = 1
-        expect_val = [['x','-','-'],['o','-','-'],['-','-','-']]
-        expect(game.place_piece(row, col)).to eq expect_val
+        expect_val = [['x', '-', '-'], ['o', '-', '-'], ['-', '-', '-']]
+        expect(game.claim(row, col)).to eq expect_val
         p game.view_grid
       end
-      # it 'with valid coordinates' do
-      #   row = 0
-      #   col = 1
-      #   expect_val = "#{row} , #{col} is already taken. Please select nother cell"
-      #   expect_val[row][col] = 'o'
-      #   expect(game.place_piece(row, col)).to eq expect_val
-      #   expect(game.current_player.name).to eq player1.name
-      # end
+      it 'with coordinates taken' do
+        row = 0
+        col = 1
+        game.claim(row, col)
+        expect_val = "#{row} , #{col} is already taken. Please select another cell"
+        expect(game.claim(row, col)).to eq expect_val
+        expect(game.current_player.name).to eq player2.name
+      end
     end
   end
   describe '#current_player' do
